b33adab4957fb186ee8ec9f3cbb52b574137fbd1
Aaron Conaway <40375373+aconaway1@users.noreply.github.com>
2023-11-17 13:51:37 -0500

    - Added public IP for VM-27-00 with A record

--- vwans.tf
+++ vwans.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_wan" "vwan00" {
-  name = "VWAN00"
+  name                = "VWAN00"
   resource_group_name = azurerm_resource_group.rg00.name
-  location = azurerm_resource_group.rg00.location
+  location            = azurerm_resource_group.rg00.location
 }
 
 
@@ -17,13 +17,13 @@ resource "azurerm_virtual_hub" "vhub00" {
 resource "azurerm_virtual_hub_connection" "vwan00-VNET27" {
   name = "VWAN00-VNET27"
 
-  virtual_hub_id = azurerm_virtual_hub.vhub00.id
+  virtual_hub_id            = azurerm_virtual_hub.vhub00.id
   remote_virtual_network_id = azurerm_virtual_network.vnet27.id
 }
 
 resource "azurerm_virtual_hub_connection" "vwan00-VNET28" {
   name = "VWAN00-VNET28"
 
-  virtual_hub_id = azurerm_virtual_hub.vhub00.id
+  virtual_hub_id            = azurerm_virtual_hub.vhub00.id
   remote_virtual_network_id = azurerm_virtual_network.vnet28.id
 }
