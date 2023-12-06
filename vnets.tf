3b6d63fe3402ab786e8cabef10ef40e1dee724a8
Aaron Conaway <40375373+aconaway1@users.noreply.github.com>
2023-12-06 14:21:08 -0500

    - Added load balancer -- Load balancer -- Address pool -- Pool association (maps VM IP to pool) -- NSG -- Interface NSG association

--- vnets.tf
+++ vnets.tf
@@ -29,28 +29,28 @@ resource "azurerm_subnet" "sub_172-27-1-0_24" {
 
 
 
-resource "azurerm_virtual_network" "vnet28" {
-  name = "VNET28"
+# resource "azurerm_virtual_network" "vnet28" {
+#   name = "VNET28"
 
-  resource_group_name = azurerm_resource_group.rg00.name
-  location            = azurerm_resource_group.rg00.location
+#   resource_group_name = azurerm_resource_group.rg00.name
+#   location            = azurerm_resource_group.rg00.location
 
-  address_space = ["172.28.0.0/16"]
+#   address_space = ["172.28.0.0/16"]
 
-}
+# }
 
-resource "azurerm_subnet" "sub_172-28-0-0_24" {
-  name                 = "SUB_172-28-0-0_24"
-  virtual_network_name = azurerm_virtual_network.vnet28.name
-  resource_group_name  = azurerm_resource_group.rg00.name
+# resource "azurerm_subnet" "sub_172-28-0-0_24" {
+#   name                 = "SUB_172-28-0-0_24"
+#   virtual_network_name = azurerm_virtual_network.vnet28.name
+#   resource_group_name  = azurerm_resource_group.rg00.name
 
-  address_prefixes = ["172.28.0.0/24"]
-}
+#   address_prefixes = ["172.28.0.0/24"]
+# }
 
-resource "azurerm_subnet" "sub_172-28-1-0_24" {
-  name                 = "SUB_172-28-1-0_24"
-  virtual_network_name = azurerm_virtual_network.vnet28.name
-  resource_group_name  = azurerm_resource_group.rg00.name
+# resource "azurerm_subnet" "sub_172-28-1-0_24" {
+#   name                 = "SUB_172-28-1-0_24"
+#   virtual_network_name = azurerm_virtual_network.vnet28.name
+#   resource_group_name  = azurerm_resource_group.rg00.name
 
-  address_prefixes = ["172.28.1.0/24"]
-}
+#   address_prefixes = ["172.28.1.0/24"]
+# }
